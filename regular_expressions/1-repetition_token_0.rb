26c2bba97ac2962e4ac8e602366203f0bc3bda4d
Larissa4-droid <l.iriza@alustudent.com>
2025-01-21 03:34:16 -0800

    1-repetition_token_0.rb

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,13 +1,2 @@
 #!/usr/bin/env ruby
-
-# Capture the first argument passed to the script
-input_string = ARGV[0]
-
-# Define the regular expression that matches strings that do not contain 'a'
-regex = /^[^a]*$/  # Matches strings with zero occurrences of 'a'
-
-# Check if the input string matches the regular expression
-if input_string.match(regex)
-  puts input_string
-end
-
+puts ARGV[0].scan(/hbt{2,5}n/)
