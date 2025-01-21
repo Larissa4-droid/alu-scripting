b106c8f5c37ea7a84c814951db2a05c54d16604e
Larissa4-droid <l.iriza@alustudent.com>
2025-01-21 03:21:58 -0800

    1-repetition_token_0.rb

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -3,8 +3,8 @@
 # Capture the first argument passed to the script
 input_string = ARGV[0]
 
-# Define the regular expression that matches zero or more repetitions of "a" (or another example)
-regex = /a{0}/
+# Define the regular expression that matches strings that do not contain 'a'
+regex = /^[^a]*$/  # Matches strings with zero occurrences of 'a'
 
 # Check if the input string matches the regular expression
 if input_string.match(regex)
